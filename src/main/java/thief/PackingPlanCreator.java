fce2ac040caf48f63d806bc17428f18a22eba077
sarahfairbairn <sarahjfairbairn@gmail.com>
2021-12-05 12:07:27 +0000

    updated packing plan so that it just adds items to knapsack until max weight is reached.

--- src/main/java/thief/PackingPlanCreator.java
+++ src/main/java/thief/PackingPlanCreator.java
@@ -13,23 +13,32 @@ public class PackingPlanCreator<C, E extends Environment> extends AntPolicy<C, E
 
     @Override
     public boolean applyPolicy(E environment, ConfigurationProvider configurationProvider) {
+
         //this gets called after every ant has finished its tour
         AntForTravellingThief a = (AntForTravellingThief)getAnt();
         List<Integer> s = a.getSolution();
-        //System.out.println("solution" + s);
         TravellingThiefEnvironment problem = (TravellingThiefEnvironment) environment;
-        //System.out.println("no of items " + problem.getTravellingThiefProblem().numOfItems);
 
-        //generate packing list of all ones just for now
+        int maxWeight = problem.getTravellingThiefProblem().maxWeight;
+        double[] itemWeights = problem.getTravellingThiefProblem().weight;
+        int weightOfKnapsack = 0;
+
+        //add items into the packing list until the max weight of the knapsack is reached
         List<java.lang.Boolean> packingPlan = new ArrayList<>(problem.getTravellingThiefProblem().numOfItems);
 
-        for (int i = 1; i <= problem.getTravellingThiefProblem().numOfItems; i++) {
-            packingPlan.add(Boolean.TRUE);
+        for (int i = 0; i < problem.getTravellingThiefProblem().numOfItems; i++) {
+            double itemWeight = itemWeights[i];
+            weightOfKnapsack += itemWeight;
+            if(weightOfKnapsack < maxWeight){
+                packingPlan.add(Boolean.TRUE);
+            }
+            else{
+                packingPlan.add(Boolean.FALSE);
+            }
         }
 
         Solution thiefSolution = problem.getTravellingThiefProblem().evaluate(s, packingPlan);
         a.setThiefSolution(thiefSolution);
-        //a.setSolution(thiefSolution);
 
         return true;
     }
